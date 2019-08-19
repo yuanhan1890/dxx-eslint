118fddfca725e64e2957d408a819bbcc3d58effa
yuanpinyang <yuanpy2009@qq.com>
2019-08-19 14:20:14 +0800

    init

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const eslintRCFile = fs.readFileSync(path.resolve(__dirname, '../.eslintrc'), 'UTF-8');
+const editorConfigFile = fs.readFileSync(path.resolve(__dirname, '../.editorconfig'), 'UTF-8');
+
+fs.writeFileSync(path.join(process.cwd(), '.eslintrc'), eslintRCFile);
+fs.writeFileSync(path.join(process.cwd(), '.editorconfig'), editorConfigFile);
