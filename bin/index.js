7983db0597be045a4e287413a3e42c305ed51b66
yuanhan1890 <yuanpy2009@qq.com>
2019-09-11 09:56:07 +0800

    feat: add eslint alias resolve

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const eslintRCFile = fs.readFileSync(path.resolve(__dirname, '../.eslintrc'), 'UTF-8');
 const editorConfigFile = fs.readFileSync(path.resolve(__dirname, '../.editorconfig'), 'UTF-8');
+const jsConfigFile = fs.readFileSync(path.resolve(__dirname, '../jsconfig.json.template'), 'UTF-8');
 
 fs.writeFileSync(path.join(process.cwd(), '.eslintrc'), eslintRCFile);
 fs.writeFileSync(path.join(process.cwd(), '.editorconfig'), editorConfigFile);
+fs.writeFileSync(path.join(process.cwd(), 'jsconfig.json'), jsConfigFile);
